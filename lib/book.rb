4f671a6d3fc2283d7947fe14a3ac0de4146ba706
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:30:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,6 +23,14 @@ class Book
     @author
   end
   
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
