e5427b6fbb2441a983ab6a31a99e3daf3ad905f7
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:36:15 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,51 @@
+class Book
+
+  def initialize(title)
+    @title = title
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+# Book.new("And Then There Were None")
 
 
+
+# book1 = Book.new("And Then There Were None", "Agatha Christie", 272, "Mystery")
+
+# p book1.turn_page
