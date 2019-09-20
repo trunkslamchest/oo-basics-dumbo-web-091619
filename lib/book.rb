967cb9329263e64fe95994c5f87a3eb953c9a43b
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:27:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,6 +8,14 @@ class Book
     # @genre = genre
   end
   
+  def title
+    @title=title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
