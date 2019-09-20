9b710e6ac622849551d1783198a4087999402fa7
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:20:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+  
+  def initialize(:title, :author, :page_count, :genre)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+  
+  attr_accessor 
+  
+  def
+  
+  end
+  
+  
+end
+
+book1 = Book.new("And Then There Were None", "Agatha Christie", 272, "Mystery")
 
 
