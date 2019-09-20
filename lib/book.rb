6cd9328866294331a908801451d773c05c0cc50e
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:24:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,9 +3,9 @@ class Book
 
   def initialize(title, author, page_count, genre)
     @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
+    # @author = author
+    # @page_count = page_count
+    # @genre = genre
   end
   
   def turn_page
