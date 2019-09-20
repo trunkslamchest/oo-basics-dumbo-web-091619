2d22ecc07813ea4f58534ee91c3edee886fa544e
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:21:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,13 +8,10 @@ class Book
     @genre = genre
   end
   
-  attr_accessor 
-  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
-  
+
 end
 
 book1 = Book.new("And Then There Were None", "Agatha Christie", 272, "Mystery")
