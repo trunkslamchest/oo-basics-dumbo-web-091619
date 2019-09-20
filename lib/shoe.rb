05acbd7a416a7c7747d8be78c7aa4436e0b2db34
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:35:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -29,5 +29,20 @@ class Shoe
     @size
   end
   
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
   
 end
