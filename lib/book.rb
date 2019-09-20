7600957a528826d01df8492b0477dd6f040382f3
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 12:24:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre
+  attr_accessor :title#, :author, :page_count, :genre
 
-  def initialize(title, author, page_count, genre)
+  def initialize(title) #, author, page_count, genre)
     @title = title
     # @author = author
     # @page_count = page_count
